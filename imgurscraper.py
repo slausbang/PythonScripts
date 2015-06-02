4f305478649922ee609a9919d19976d73f62b0c0
slausbang <slausbang@users.noreply.github.com>
2015-06-01 19:33:28 -0700

    Update imgurscraper.py
    Added ability to support multiple subreddits with saving in individual directories.

--- imgurscraper.py
+++ imgurscraper.py
@@ -1,13 +1,21 @@
 from imgurpython import ImgurClient
 import wget, time, datetime, os
 
-#gather variables
+#declare globals
 today = datetime.date.today()
 todaystr = today.isoformat()
-subreddit = input('Enter subreddit: ')
+
+string_input = input('Enter subreddit(s), separated by spaces: ')
+input_list = string_input.split()
+input_list = [str(a) for a in input_list]
 timeperiod = input('Enter Timeperiod (week/day): ')
 
-#create directories
+#imgur auth data
+client_id = '1cd4ab0d955461d'
+client_secret = '9ed65fc5d90719d0ab5875ac2f0099cf8c79d0c4'
+client = ImgurClient(client_id, client_secret)
+
+#create directory
 if not os.path.exists(todaystr):
 	print('creating directory..' + todaystr)
 	os.mkdir(todaystr)
@@ -15,19 +23,24 @@ if not os.path.exists(todaystr):
 elif os.path.exists(todaystr):
 	os.chdir(todaystr)
 
-if not os.path.exists(subreddit):
-	print ('creating directory..' + subreddit)
-	os.mkdir(subreddit)
-	os.chdir(subreddit)
-elif os.path.exists(subreddit):
-	os.chdir(subreddit)
+#function to download the items we decide
+def downloadItems(subreddit, timep):
+	global client
+	items=client.subreddit_gallery(subreddit, sort='time', window=timep)
+	for item in items:
+		wget.download(item.link)
 
-#begin imgur collection section
-client_id = '1cd4ab0d955461d'
-client_secret = '9ed65fc5d90719d0ab5875ac2f0099cf8c79d0c4'
 
-client = ImgurClient(client_id, client_secret)
-
-items=client.subreddit_gallery(subreddit, sort='time', window=timeperiod)
-for item in items:
-	wget.download(item.link)
+for i in input_list:
+	if not os.path.exists(i):
+		print('creating directory..' + i)
+		os.mkdir(i)
+		os.chdir(i)
+		downloadItems(i, timeperiod)
+	else:
+		os.chdir(i)
+		downloadItems(i, timeperiod)
+	os.chdir('../')
+	
+	
+		
